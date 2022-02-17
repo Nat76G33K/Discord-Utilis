ae672fadd57bc74a45aa4b3aa860df598b52d0ae
Nat76 <nat76geek@gmail.com>
2022-02-17 13:46:42 +0400

    Modified ping

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+'use strict';
 
-const defaultPrefix = "!";
+const {Client, MessageEmbed, Intents} = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS]});
 
+var defaultPrefix = "!";
 
 client.login(process.env.token);
 
@@ -11,6 +12,7 @@ client.on("ready", async () => {
 
   try {
     client.guilds.cache.get("939857766397395004").channels.cache.get("939931822039638116").send("Ready to work <@754229847206658160> !");
+    client.user.setActivity("Nat76 coding myself.", { type: "WATCHING" });
   } catch (err) {
     console.log(`Failed to start : ${err}`);
   }
@@ -18,19 +20,36 @@ client.on("ready", async () => {
   console.log("Sucessfully started.");
 })
 
- client.on("messageCreate", async message => {
+client.on("messageCreate", async msg => {
 
-  if (message.content.startsWith(defaultPrefix) && message.channel !== "DMChannel" || "PartialDMChannel") {
+  if (msg.content.startsWith(defaultPrefix) && msg.channel.type !== "DM" && !msg.author.bot) {
 
-    const command = message.content.slice(1);
-    const channel = message.channel;
+    const cmd = msg.content.slice(1).toLowerCase();
+    const channel = msg.channel;
 
-    if (command === "ping") {
-      
-      const ping = Date.now() - message.createdTimestamp;
-      message.react("🏓");
-      channel.send(` Pong !\n\`${ping}ms\``);
-      
+    if (cmd === "ping") {
+
+      const ping = msg.createdTimestamp - Date.now();
+      let i;
+      let e;
+      const cmt = ["Nice ping :sparkles::sparkles:!", "Rip lags...", "Bruh"]
+
+      if (ping < 100) {
+        i = "#40ff00";
+        e = 0;
+      } else if (ping > 100 && ping < 320) {
+        i = "#ff6f00";
+        e = 1;
+      } else if (ping > 320) {
+        i = "#ff0000";
+        e = 2;
+      }
+
+      const embed = new MessageEmbed().setColor(i).setDescription("Your ping is `" + ping + "ms\`.\n" + cmt[e]);
+
+      msg.react("🏓");
+      await channel.send({embeds: [embed]});
+
     }
   }
 
